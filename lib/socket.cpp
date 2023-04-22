74556fcc15efadefd5144086ae1553253e976515
Amir-Int <amir.intelli@gmail.com>
2023-04-22 22:44:36 +0100

    lib/socket.hpp:_PLATFORM_WINDOWS::socket_interface::Socket: Move method definitions seperately to the implementation file.

---
+++ lib/socket.cpp
@@ -0,0 +1,143 @@
+#include "socket.hpp"
+
+
+#ifdef _PLATFORM_WINDOWS
+
+namespace socket_interface
+{
+
+Socket::Socket()
+{
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+        std::cerr << "Initialisation failed" << std::endl;
+}
+
+Socket::~Socket()
+{
+
+}
+
+SOCKET Socket::getSocket(int af, int type, int protocol)
+{
+    SOCKET socketDsc = socket(af, type, protocol);
+    if (socketDsc == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed" << std::endl;
+        WSACleanup();
+        exit(1);
+    }
+    return socketDsc;
+}
+
+
+void getBinded(SOCKET s, const sockaddr *addr, int addrlen)
+{
+    if (bind(s, addr, addrlen) == SOCKET_ERROR) {
+        std::cerr << "Socket binding failed" << std::endl;
+        closesocket(s);
+        WSACleanup();
+        exit(1);
+    }
+}
+
+void startListening(SOCKET s, int backlog)
+{
+    if (listen(s, backlog) == SOCKET_ERROR) {
+        std::cerr << "Listening failed" << std::endl;
+        closesocket(s);
+        WSACleanup();
+        exit(1);
+    }
+}
+
+int acceptClient(SOCKET s, sockaddr *addr, int *addrlen)
+{
+    int newSocketDsc = accept(s, addr, addrlen);
+    if (newSocketDsc == INVALID_SOCKET) {
+        std::cerr << "Accepting client failed" << std::endl;
+        closesocket(s);
+        WSACleanup();
+        exit(1);
+    }
+    return newSocketDsc;
+}
+
+void getConnected(SOCKET s, const sockaddr *name, int namelen)
+{
+    if (connect(s, name, namelen) == SOCKET_ERROR) {
+        std::cerr << "Could not connect to the server" << std::endl;
+        exit(1);
+    }
+}
+
+int sendMessage(SOCKET s, const char *buf, int len, int flags)
+{
+    int sentBytesLen = send(s, buf, len, flags);
+    if (sentBytesLen == SOCKET_ERROR)
+        return -1;
+    return sentBytesLen;
+}
+
+int recvMessage(SOCKET s, char *buf, int len, int flags)
+{
+    int recvedBytesLen = recv(s, buf, len, flags);
+    int errorNo = WSAGetLastError();
+    if (errorNo == WSAEWOULDBLOCK || errorNo == WSAECONNRESET)
+        return errorNo;
+    if (recvedBytesLen == SOCKET_ERROR) {
+        std::cerr << "Reading failed" << std::endl;
+        return -1;
+    }
+    return recvedBytesLen;
+}
+
+void closeSocket(SOCKET s)
+{
+    if (closesocket(s) == SOCKET_ERROR) {
+        std::cerr << "Closing connection failed" << std::endl;
+        WSACleanup();
+        exit(1);
+    }
+    WSACleanup();
+}
+
+void getAddrInfo(
+    PCSTR pNodeName
+    , PCSTR pServiceName
+    , const ADDRINFOA *pHints
+    , ADDRINFOA **ppResult)
+{
+    if (getaddrinfo(pNodeName, pServiceName, pHints, ppResult) != 0) {
+        std::cerr << "Translation failed" << std::endl;
+        WSACleanup();
+        exit(1);
+    }
+}
+
+int switchMode(SOCKET s, u_long *mode)
+{
+    if (ioctlsocket(s, FIONBIO, mode) == SOCKET_ERROR) {
+        std::cerr << "Switching to new mode failed" << std::endl;
+        closesocket(s);
+        WSACleanup();
+        return 1;
+    }
+    return 0;
+}
+
+} // namespace socket_interface
+
+#endif
+
+#ifdef _PLATFORM_LINUX
+
+namespace socket_interface
+{
+
+Socket::Socket(int af, int type, int protocol)
+{
+
+}
+
+} // namespace socket_interface
+
+#endif
--- lib/socket.hpp
+++ lib/socket.hpp
@@ -7,6 +7,9 @@
 #include <Ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
+namespace socket_interface
+{
+
 /**
  * \class Socket
  * \brief The Socket class provides an interface to use the
@@ -14,107 +17,36 @@
 */
 class Socket {
     public:
-        Socket() {
-            if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-                std::cerr << "Initialisation failed" << std::endl;
-        }
+        Socket();
 
-        SOCKET getSocket(int af, int type, int protocol) {
-            SOCKET socketDsc = socket(af, type, protocol);
-            if (socketDsc == INVALID_SOCKET) {
-                std::cerr << "Socket creation failed" << std::endl;
-                WSACleanup();
-                exit(1);
-            }
-            return socketDsc;
-        }
+        ~Socket();
 
-        void getBinded(SOCKET s, const sockaddr *addr, int addrlen) {
-            if (bind(s, addr, addrlen) == SOCKET_ERROR) {
-                std::cerr << "Socket binding failed" << std::endl;
-                closesocket(s);
-                WSACleanup();
-                exit(1);
-            }
-        }
+        SOCKET getSocket(int af, int type, int protocol);
 
-        void startListening(SOCKET s, int backlog) {
-            if (listen(s, backlog) == SOCKET_ERROR) {
-                std::cerr << "Listening failed" << std::endl;
-                closesocket(s);
-                WSACleanup();
-                exit(1);
-            }
-        }
+        void getBinded(SOCKET s, const sockaddr *addr, int addrlen);
 
-        int acceptClient(SOCKET s, sockaddr *addr, int *addrlen) {
-            int newSocketDsc = accept(s, addr, addrlen);
-            if (newSocketDsc == INVALID_SOCKET) {
-                std::cerr << "Accepting client failed" << std::endl;
-                closesocket(s);
-                WSACleanup();
-                exit(1);
-            }
-            return newSocketDsc;
-        }
+        void startListening(SOCKET s, int backlog);
 
-        void getConnected(SOCKET s, const sockaddr *name, int namelen) {
-            if (connect(s, name, namelen) == SOCKET_ERROR) {
-                std::cerr << "Could not connect to the server" << std::endl;
-                exit(1);
-            }
-        }
+        int acceptClient(SOCKET s, sockaddr *addr, int *addrlen);
 
-        int sendMessage(SOCKET s, const char *buf, int len, int flags) {
-            int sentBytesLen = send(s, buf, len, flags);
-            if (sentBytesLen == SOCKET_ERROR)
-                return -1;
-            return sentBytesLen;
-        }
+        void getConnected(SOCKET s, const sockaddr *name, int namelen);
+
+        int sendMessage(SOCKET s, const char *buf, int len, int flags);
 
         // Terminates the programme if the peer closes the connection.
-        int recvMessage(SOCKET s, char *buf, int len, int flags) {
-            int recvedBytesLen = recv(s, buf, len, flags);
-            int errorNo = WSAGetLastError();
-            if (errorNo == WSAEWOULDBLOCK || errorNo == WSAECONNRESET)
-                return errorNo;
-            if (recvedBytesLen == SOCKET_ERROR) {
-                std::cerr << "Reading failed" << std::endl;
-                return -1;
-            }
-            return recvedBytesLen;
-        }
+        int recvMessage(SOCKET s, char *buf, int len, int flags);
 
-        void closeSocket(SOCKET s) {
-            if (closesocket(s) == SOCKET_ERROR) {
-                std::cerr << "Closing connection failed" << std::endl;
-                WSACleanup();
-                exit(1);
-            }
-            WSACleanup();
-        }
+        void closeSocket(SOCKET s);
 
-        void getAddrInfo(PCSTR pNodeName, PCSTR pServiceName, const ADDRINFOA *pHints, ADDRINFOA **ppResult) {
-            if (getaddrinfo(pNodeName, pServiceName, pHints, ppResult) != 0) {
-                std::cerr << "Translation failed" << std::endl;
-                WSACleanup();
-                exit(1);
-            }
-        }
+        void getAddrInfo(PCSTR pNodeName, PCSTR pServiceName, const ADDRINFOA *pHints, ADDRINFOA **ppResult);
 
-        int switchMode(SOCKET s, u_long *mode) {
-            if (ioctlsocket(s, FIONBIO, mode) == SOCKET_ERROR) {
-                std::cerr << "Switching to new mode failed" << std::endl;
-                closesocket(s);
-                WSACleanup();
-                return 1;
-            }
-            return 0;
-        }
+        int switchMode(SOCKET s, u_long *mode);
 
     private:
         WSADATA wsaData;
 };
+
+}
 
 #endif
 
@@ -171,5 +103,5 @@ class Socket {
         int addrlen{ sizeof(address) };
 };
 
-}
+} // namespace socket_interface
 #endif
